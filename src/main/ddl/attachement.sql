ff423baf835e2302d380d995cf0a01c7f2dba8cc
kiriumi <kiriumi.miko@gmail.com>
2022-09-25 01:15:25 +0900

    DBのDDLを追加

---
+++ src/main/ddl/attachement.sql
@@ -0,0 +1,6 @@
+CREATE TABLE public.attachement
+(
+    id                          TEXT,
+    filename                    TEXT,
+    attachement                 BYTEA
+);
---
+++ src/main/ddl/mail.sql
@@ -0,0 +1,4 @@
+CREATE TABLE public.mail
+(
+    id                          TEXT
+);
